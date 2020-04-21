4197af12bf02c8fde463ec395aec13c533fb2889
KyleY <kyle.yu.96@outlook.com>
2020-04-21 18:13:24 -0400

    small fix

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,8 +2,9 @@ class Movie < ApplicationRecord
   has_many :rentals
   has_many :movie_categories
   has_many :categories, through: :movie_categories
-  validates :title, :year, :summary, presence: true
+  validates :title, :year, :summary, :poster_url, presence: true
   validates :max_quantity, numericality: { greater_than: 0 }
   validates :current_inventory, numericality: { greater_than_or_equal_to: 0 }
-  default_scope -> { order(created_at: :desc) }
+  default_scope -> { order(year: :desc, created_at: :desc) }
+  default_scope -> { order(last_name: :asc, first_name: :asc) }
 end
